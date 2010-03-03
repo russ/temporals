337494e2e2a2df5621e524fb1d2140e337f3e089
Russ Smith <russ@bashme.org>
2010-03-03 14:05:14 -0800

    Adding Saturday to WDay order.
    Was making occurs_on_day? return true no matter what.
    
    Example:
    Temporal.parse('every saturday at 2pm').occurs_on_day?(Time.parse('2010-03-02')) => true

--- lib/temporals/types.rb
+++ lib/temporals/types.rb
@@ -26,7 +26,7 @@ class Temporal
   end
 
   class WDay < Classification
-    @order = %w(Sunday Monday Tuesday Wednesday Thursday Friday)
+    @order = %w(Sunday Monday Tuesday Wednesday Thursday Friday Saturday)
     @translations = {
       'S' => 'Sunday',
       'M' => 'Monday',
